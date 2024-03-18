ab582919e6925a37157bc1a6dd2dbbc3d42164c8
Galaxy_ <suyogmaharjan38@gmail.com>
2024-03-18 09:49:13 +0545

    Acoordion

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,102 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+
+const question = [
+	{
+		question: "How many team members can I invite?",
+		answer:
+			"You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
+	},
+	{
+		question: "What is the maximum file upload size?",
+		answer:
+			"No more than 2GB. All files in your account must fit your allotted storage space.",
+	},
+	{
+		question: "How do I reset my password?",
+		answer:
+			"Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.",
+	},
+	{
+		question: "Can I cancel my subscription?",
+		answer:
+			"Yes! Send us a message and we’ll process your request no questions asked.",
+	},
+	{
+		question: "Do you provide additional support?",
+		answer:
+			"Chat and email support is available 24/7. Phone lines are open during normal business hours.",
+	},
+];
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	let [currentOpen, setcurrentOpen] = useState(-1);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	function handleClick(i) {
+		if (i === currentOpen) setcurrentOpen(-1);
+		else setcurrentOpen(i);
+	}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<main className="min-h-dvh justify-center flex p-2 align-middle items-center ">
+			<div className="bg-white lg:w-3/4 md:p-14 md:pl-0 p-10 rounded-2xl flex md:flex-row flex-col relative object-contain drop-shadow-2xl overflow-hidden gap-9">
+				<div className=" items-center justify-center flex  md:w-1/2 relative">
+					<img className=" " src="/illustration-woman-online-desktop.svg" />
+					<img
+						className="absolute aspect-[46/45] w-[450px]"
+						src="/bg-pattern-desktop.svg"
+					/>
+				</div>
+				<div className="gap-10 flex-col flex w-full md:w-1/2">
+					<h1 className="font-semibold text-3xl ">FAQ</h1>
+					<div className="gap-6 flex flex-col transition ease-in-out">
+						{question.map((data, i) => {
+							return (
+								<div
+									className="flex flex-col gap-4 transition ease-in-out duration-300"
+									key={i}>
+									<div
+										className={
+											"flex justify-between items-center cursor-pointer hover:text-orange-300 transition ease-in-out"
+										}
+										onClick={() => {
+											handleClick(i);
+											focus();
+										}}>
+										<h3
+											className={
+												"text-lg text-inherit transition ease-in-out" +
+												(currentOpen === i ? " font-bold" : "")
+											}>
+											{data.question}
+										</h3>
+										<img
+											src="/icon-arrow-down.svg"
+											className={
+												"w-[14px] h-[10px]  cursor-pointer transition ease-in-out" +
+												(currentOpen === i
+													? " transform rotate-180 transition ease-in-out duration-300"
+													: "")
+											}
+										/>
+									</div>
+									<div>
+										<div
+											className={
+												"text-gray-500  overflow-hidden transition ease-in-out duration-300 " +
+												(currentOpen === i ? "max-h-[100%]" : "max-h-0")
+											}>
+											{data.answer}
+										</div>
+									</div>
+									<hr />
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
