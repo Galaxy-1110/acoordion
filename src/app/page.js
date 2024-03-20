1869b5713ca8e6d9cc63a6bc060a9d9d9906ced1
Galaxy_ <suyogmaharjan38@gmail.com>
2024-03-20 18:20:14 +0545

    smooth transition

--- src/app/page.js
+++ src/app/page.js
@@ -40,7 +40,7 @@ export default function Home() {
 
 	return (
 		<main className="min-h-dvh justify-center flex p-2 align-middle items-center ">
-			<div className="bg-white lg:w-3/4 md:p-14 md:pl-0 p-10 rounded-2xl flex md:flex-row flex-col relative object-contain drop-shadow-2xl overflow-hidden gap-9">
+			<div className="bg-white max-h-fit lg:w-3/4 md:p-14 md:pl-0 p-10 rounded-2xl flex md:flex-row flex-col relative object-contain drop-shadow-2xl  transition-all ease-linear duration-500 overflow-hidden gap-9">
 				<div className=" items-center justify-center flex  md:w-1/2 relative">
 					<img className=" " src="/illustration-woman-online-desktop.svg" />
 					<img
@@ -50,15 +50,13 @@ export default function Home() {
 				</div>
 				<div className="gap-10 flex-col flex w-full md:w-1/2">
 					<h1 className="font-semibold text-3xl ">FAQ</h1>
-					<div className="gap-6 flex flex-col transition ease-in-out">
+					<div className="gap-6 flex flex-col ">
 						{question.map((data, i) => {
 							return (
-								<div
-									className="flex flex-col gap-4 transition ease-in-out duration-300"
-									key={i}>
+								<div className="flex flex-col gap-4" key={i}>
 									<div
 										className={
-											"flex justify-between items-center cursor-pointer hover:text-orange-300 transition ease-in-out"
+											"flex justify-between items-center cursor-pointer hover:text-orange-300 transition-all ease-in-out"
 										}
 										onClick={() => {
 											handleClick(i);
@@ -66,7 +64,7 @@ export default function Home() {
 										}}>
 										<h3
 											className={
-												"text-lg text-inherit transition ease-in-out" +
+												"text-lg text-inherit transition-all ease-in-out" +
 												(currentOpen === i ? " font-bold" : "")
 											}>
 											{data.question}
@@ -74,22 +72,22 @@ export default function Home() {
 										<img
 											src="/icon-arrow-down.svg"
 											className={
-												"w-[14px] h-[10px]  cursor-pointer transition ease-in-out" +
+												"w-[14px] h-[10px]  cursor-pointer transition-all ease-in-out" +
 												(currentOpen === i
-													? " transform rotate-180 transition ease-in-out duration-300"
+													? " transform rotate-180 transition-all ease-in-out duration-300"
 													: "")
 											}
 										/>
 									</div>
-									<div>
-										<div
-											className={
-												"text-gray-500  overflow-hidden transition ease-in-out duration-300 " +
-												(currentOpen === i ? "max-h-[100%]" : "max-h-0")
-											}>
-											{data.answer}
-										</div>
+
+									<div
+										className={
+											"text-gray-500  overflow-hidden grid transition-all ease-in-out duration-[500ms] " +
+											(currentOpen === i ? "grid-rows-[1fr]" : "grid-rows-[0]")
+										}>
+										{data.answer}
 									</div>
+
 									<hr />
 								</div>
 							);
